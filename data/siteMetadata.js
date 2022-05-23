07d62fb8e503e916ed68e2084405e8ca7d5cfa3a
michal <michal.d.gornicki@gmail.com>
2022-05-23 22:41:42 +0100

    update socialBanner

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -9,7 +9,7 @@ const siteMetadata = {
   siteRepo: 'https://github.com/m1ner79/blogspace',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/banner.png',
+  socialBanner: '/static/images/twitter-card.png',
   email: 'michal.d.gornicki@gmail.com',
   github: 'https://github.com/m1ner79?tab=repositories',
   //twitter: 'https://twitter.com/Twitter',
